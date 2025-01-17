a42cee1a46fc1b6e8e6d1261bd9f1028273a2fa4
Niel22 <niel2264@gmail.com>
2025-01-17 07:57:03 +0100

    Fix a bug in the auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,37 +1,35 @@
-const models = require('../models');
-const jwt = require('jsonwebtoken');
-const { error } = require('../utils/apiResponse');
+const models = require("../models");
+const jwt = require("jsonwebtoken");
+const { error } = require("../utils/apiResponse");
 
-async function authMiddleware(req, res, next)
-{
-    let token;
+async function authMiddleware(req, res, next) {
+  let token;
 
-    if(req?.headers?.authorization?.startsWith("Bearer"))
-    {
-        token  = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
-        
-        if(!decoded.userId)
-        {
-            return error(res, "UnAuthenticated");
-        }
-        
-        const user = await models.User.findByPk(decoded?.userId);
+  if (req?.headers?.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
 
-        if(user.refreshToken !== null){
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
 
-            
-            req.userData = user;
-            // console.log(req.userData.id);
-            
-            next();
-            return;
-        }
+      if (!decoded.userId) {
+        return error(res, "UnAuthenticated");
+      }
+
+      const user = await models.User.findByPk(decoded?.userId);
+
+      if (user.refreshToken !== null) {
+        req.userData = user;
+        // console.log(req.userData.id);
+
+        next();
+        return;
+      }
+    } catch (err) {
+      return error(res, err.message);
     }
+  }
 
-    return error(res, "Unauthenticated");
-
-
+  return error(res, "Unauthenticated");
 }
 
 module.exports = authMiddleware;
